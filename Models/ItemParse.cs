773db63815ab263d1bd35c9a66bee9617a55e712
Vladimir <urkytsk3@gmail.com>
2023-03-06 16:06:08 +0800

    Добавил библиотеку MaterialDesign и ресурсы к ней

---
+++ Models/ItemParse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoRuScrapper.Models
+{
+    public class ItemParse
+    {
+        public int Id { get; set; }
+        public string Marka { get; set; }
+        public string Model { get; set; }
+        public DateTime DateRevision { get; set; }
+
+        public ItemParse() { }
+
+        public ItemParse(string marka, string model, DateTime dateRevision)
+        {
+            this.Marka = marka;
+            this.Model = model;
+            DateRevision = dateRevision;
+        }
+    }
+}
