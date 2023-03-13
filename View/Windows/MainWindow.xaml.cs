6f47253131a5d0c868c18078064bb509a6fce1c3
Vladimir <urkytsk3@gmail.com>
2023-03-13 13:51:50 +0800

    Перенес все свойства и меотды добавления марок и регионов в MainWindow

--- View/Windows/MainWindow.xaml.cs
+++ View/Windows/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using AutoRuScrapper.Models;
 using AutoRuScrapper.Resources;
 using AutoRuScrapper.ViewModels;
+using MaterialDesignThemes.Wpf;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -17,6 +18,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using AutoRuScrapper.ViewModels.Base;
+using Newtonsoft.Json;
 
 namespace AutoRuScrapper
 {
@@ -28,10 +31,64 @@ namespace AutoRuScrapper
         public MainWindow()
         {
             InitializeComponent();
-
+            this.DataContext = this;
         }
 
+        #region Марки машин
+        private Mark _selectedMark;
+        public Mark SelectedMark
+        {
+            get => _selectedMark;
+            set => _selectedMark = value;
+        }
 
+        private List<Mark> _listMarks;
+        public List<Mark> ListMarks
+        {
+            get => _listMarks;
+            set => _listMarks = value;
+        }
+
+        #endregion
+
+        #region Регионы
+
+        private Region _selectedRegion;
+        public Region SelectedRegion
+        {
+            get => _selectedRegion;
+            set => _selectedRegion = value;
+        }
+
+        private List<Region> _listMainRegions;
+        public List<Region> ListMainRegions
+        {
+            get => _listMainRegions;
+            set => _listMainRegions = value;
+        }
+
+        private List<Region> _listSubRegions;
+        public List<Region> ListSubRegions
+        {
+            get => _listSubRegions;
+            set => _listSubRegions = value;
+        }
+
+        private List<Region> _allRegions;
+        public List<Region> AllRegions
+        {
+            get => _allRegions;
+            set => _allRegions = value;
+        }
+
+        #endregion
+
+        private string _pareserUrl;
+        public string ParserUrl
+        {
+            get => _pareserUrl;
+            set => _pareserUrl = value;
+        }
 
         private void CreateItem_Click(object sender, RoutedEventArgs e)
         {
@@ -63,5 +120,59 @@ namespace AutoRuScrapper
             gridContainer.Children.Add(newItem);
         }
 
+        private void StartParser_Click(object sender, RoutedEventArgs e)
+        {
+            Mark mark = SelectedMark;
+            Region region = SelectedRegion;
+            string asdf = ParserUrl;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            #region Добавляю список марок машин
+            try
+            {
+                string carMarksjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Car\Marks.json");
+                ListMarkCars? listMarkCars = JsonConvert.DeserializeObject<ListMarkCars>(carMarksjson);
+
+                if (listMarkCars?.Marks != null)
+                    ListMarks = listMarkCars.Marks;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            #endregion
+
+            #region Добавляю регионы
+
+            try
+            {
+                #region Основные регионы
+                string mainRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\MainRegions.json");
+                ListMainRegions? listMainRegions = JsonConvert.DeserializeObject<ListMainRegions>(mainRegionjson);
+
+                if (listMainRegions?.MainRegions != null)
+                    ListMainRegions = listMainRegions.MainRegions;
+                #endregion
+
+                #region Области
+                string subRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\SubRegions.json");
+                ListSubRegions? listSubRegions = JsonConvert.DeserializeObject<ListSubRegions>(subRegionjson);
+
+                if (listSubRegions?.SubRegions != null)
+                    ListSubRegions = listSubRegions.SubRegions;
+                #endregion
+
+                // Объяденяю регионы
+                AllRegions = ListMainRegions.Concat(ListSubRegions).ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            #endregion
+        }
     }
 }
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,169 +1,171 @@
-﻿using AutoRuScrapper.Commands;
-using AutoRuScrapper.Models;
-using AutoRuScrapper.Resources;
-using AutoRuScrapper.ViewModels.Base;
-using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.ComponentModel.Design;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using Microsoft.Extensions.DependencyInjection;
+﻿//using AutoRuScrapper.Commands;
+//using AutoRuScrapper.Models;
+//using AutoRuScrapper.Resources;
+//using AutoRuScrapper.ViewModels.Base;
+//using Newtonsoft.Json;
+//using System;
+//using System.Collections.Generic;
+//using System.Collections.ObjectModel;
+//using System.ComponentModel.Design;
+//using System.IO;
+//using System.Linq;
+//using System.Text;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+//using System.Windows;
+//using System.Windows.Controls;
+//using System.Windows.Input;
+//using Microsoft.Extensions.DependencyInjection;
 
 
-namespace AutoRuScrapper.ViewModels
-{
-    internal class MainWindowViewModel : BaseViewModel
-    {
-        #region Марки машин
-        private Mark _selectedMark;
-        public Mark SelectedMark
-        {
-            get => _selectedMark;
-            set => Set(ref _selectedMark, value);
-        }
+//namespace AutoRuScrapper.ViewModels
+//{
+//    internal class MainWindowViewModel : BaseViewModel
+//    {
+//        #region Марки машин
+//        private Mark _selectedMark;
+//        public Mark SelectedMark
+//        {
+//            get => _selectedMark;
+//            set => Set(ref _selectedMark, value);
+//        }
 
-        private List<Mark> _listMarks;
-        public List<Mark> ListMarks
-        {
-            get => _listMarks;
-            set => Set(ref _listMarks, value);
-        }
+//        private List<Mark> _listMarks;
+//        public List<Mark> ListMarks
+//        {
+//            get => _listMarks;
+//            set => Set(ref _listMarks, value);
+//        }
 
-        #endregion
+//        #endregion
 
-        #region Регионы
+//        #region Регионы
 
-        private Region _selectedRegion;
-        public Region SelectedRegion
-        {
-            get => _selectedRegion;
-            set => Set(ref _selectedRegion, value);
-        }
+//        private Region _selectedRegion;
+//        public Region SelectedRegion
+//        {
+//            get => _selectedRegion;
+//            set => Set(ref _selectedRegion, value);
+//        }
 
-        private List<Region> _listMainRegions;
-        public List<Region> ListMainRegions
-        {
-            get => _listMainRegions;
-            set => Set(ref _listMainRegions, value);
-        }
+//        private List<Region> _listMainRegions;
+//        public List<Region> ListMainRegions
+//        {
+//            get => _listMainRegions;
+//            set => Set(ref _listMainRegions, value);
+//        }
 
-        private List<Region> _listSubRegions;
-        public List<Region> ListSubRegions
-        {
-            get => _listSubRegions;
-            set => Set(ref _listSubRegions, value);
-        }
+//        private List<Region> _listSubRegions;
+//        public List<Region> ListSubRegions
+//        {
+//            get => _listSubRegions;
+//            set => Set(ref _listSubRegions, value);
+//        }
 
-        private List<Region> _allRegions;
-        public List<Region> AllRegions
-        {
-            get => _allRegions;
-            set => Set(ref _allRegions, value);
-        }
+//        private List<Region> _allRegions;
+//        public List<Region> AllRegions
+//        {
+//            get => _allRegions;
+//            set => Set(ref _allRegions, value);
+//        }
 
-        #endregion
+//        #endregion
 
-        private string _title = "Автору парсер";
-        public string Title
-        {
-            get => _title;
-            set => Set(ref _title, value);
-        }
+//        private string _title = "Автору парсер";
+//        public string Title
+//        {
+//            get => _title;
+//            set => Set(ref _title, value);
+//        }
 
-        #region Парсер
-        private string _pareserUrl;
-        public string PareserUrl
-        {
-            get => _pareserUrl;
-            set => Set(ref _pareserUrl, value);
-        }
+//        #region Парсер
 
-        private List<ItemParser> _itemParsers;
-        public List<ItemParser> ItemParsers
-        {
-            get => _itemParsers;
-            set => Set(ref _itemParsers, value);
-        }
-        #endregion
+//        private string _pareserUrl;
+//        public string ParserUrl
+//        {
+//            get => _pareserUrl;
+//            set => Set(ref _pareserUrl, value);
+//        }
 
-
-
-        #region Комманды
-        public ICommand StartParserCommand { get; }
-
-        private bool CanStartParserCommandExecute(object p) => true;
-        public void OnStartParserCommandExecuted(object p)
-        {
-            MessageBox.Show("START!");
-        }
+//        private List<string> _parsers;
+//        public List<string> Parsers
+//        {
+//            get => _parsers;
+//            set => Set(ref _parsers, value);
+//        }
+//        #endregion
 
 
-        #endregion
+
+//        #region Комманды
+//        public ICommand StartParserCommand { get; }
+
+//        private bool CanStartParserCommandExecute(object p) => true;
+//        public void OnStartParserCommandExecuted(object p)
+//        {
+//            string asdf = ParserUrl.Trim();
+//            MessageBox.Show("START!");
+//        }
 
 
-        public MainWindowViewModel()
-        {
+//        #endregion
 
-            #region Добавляю список марок машин
-            try
-            {
-                string carMarksjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Car\Marks.json");
-                ListMarkCars? listMarkCars = JsonConvert.DeserializeObject<ListMarkCars>(carMarksjson);
 
-                if (listMarkCars?.Marks != null)
-                    ListMarks = listMarkCars.Marks;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            #endregion
+//        public MainWindowViewModel()
+//        {
 
-            #region Добавляю регионы
+//            #region Добавляю список марок машин
+//            try
+//            {
+//                string carMarksjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Car\Marks.json");
+//                ListMarkCars? listMarkCars = JsonConvert.DeserializeObject<ListMarkCars>(carMarksjson);
 
-            try
-            {
-                #region Основные регионы
-                string mainRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\MainRegions.json");
-                ListMainRegions? listMainRegions = JsonConvert.DeserializeObject<ListMainRegions>(mainRegionjson);
+//                if (listMarkCars?.Marks != null)
+//                    ListMarks = listMarkCars.Marks;
+//            }
+//            catch (Exception ex)
+//            {
+//                MessageBox.Show(ex.Message);
+//            }
+//            #endregion
 
-                if (listMainRegions?.MainRegions != null)
-                    ListMainRegions = listMainRegions.MainRegions;
-                #endregion
+//            #region Добавляю регионы
 
-                #region Области
-                string subRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\SubRegions.json");
-                ListSubRegions? listSubRegions = JsonConvert.DeserializeObject<ListSubRegions>(subRegionjson);
+//            try
+//            {
+//                #region Основные регионы
+//                string mainRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\MainRegions.json");
+//                ListMainRegions? listMainRegions = JsonConvert.DeserializeObject<ListMainRegions>(mainRegionjson);
 
-                if (listSubRegions?.SubRegions != null)
-                    ListSubRegions = listSubRegions.SubRegions;
-                #endregion
+//                if (listMainRegions?.MainRegions != null)
+//                    ListMainRegions = listMainRegions.MainRegions;
+//                #endregion
 
-                // Объяденяю регионы
-                AllRegions = ListMainRegions.Concat(ListSubRegions).ToList();
-            }
-            catch (Exception)
-            {
-                throw;
-            }
+//                #region Области
+//                string subRegionjson = File.ReadAllText(@"Z:\Programming\ProjectC#\AutoRuScrapper\Data\Regions\SubRegions.json");
+//                ListSubRegions? listSubRegions = JsonConvert.DeserializeObject<ListSubRegions>(subRegionjson);
 
-            #endregion
+//                if (listSubRegions?.SubRegions != null)
+//                    ListSubRegions = listSubRegions.SubRegions;
+//                #endregion
 
-            #region Создание и добавление парсера
-            #endregion
+//                // Объяденяю регионы
+//                AllRegions = ListMainRegions.Concat(ListSubRegions).ToList();
+//            }
+//            catch (Exception)
+//            {
+//                throw;
+//            }
 
-            #region Вызов комманд
-            StartParserCommand = new LambdaCommand(OnStartParserCommandExecuted, CanStartParserCommandExecute);
+//            #endregion
 
-            #endregion
-        }
-    }
-}
+//            #region Создание и добавление парсера
+//            #endregion
+
+//            #region Вызов комманд
+//            StartParserCommand = new LambdaCommand(OnStartParserCommandExecuted, CanStartParserCommandExecute);
+
+//            #endregion
+//        }
+//    }
+//}
